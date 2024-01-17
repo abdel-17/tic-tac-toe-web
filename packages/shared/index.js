7ba0862cdf6bcb46620715232a64d304495eafbf
abdel-17 <abdelrahmanb015@gmail.com>
2024-01-17 16:50:08 +0200

    style: rename function

--- packages/shared/index.js
+++ packages/shared/index.js
@@ -21,26 +21,25 @@ export function determineWinner({ board, currentPlayer, lastPlayedAt }) {
 	//  3 | 4 | 5
 	// ---|---|---
 	//  6 | 7 | 8
-
 	const row = Math.floor(lastPlayedAt / 3);
 	const column = lastPlayedAt % 3;
 
 	// Check the row triplet
-	if (areEqual(board[3 * row], board[3 * row + 1], board[3 * row + 2])) {
+	if (allEqual(board[3 * row], board[3 * row + 1], board[3 * row + 2])) {
 		return currentPlayer;
 	}
 
 	// Check the column triplet
-	if (areEqual(board[column], board[column + 3], board[column + 6])) {
+	if (allEqual(board[column], board[column + 3], board[column + 6])) {
 		return currentPlayer;
 	}
 
 	// Check the diagonal triplets
-	if (row === column && areEqual(board[0], board[4], board[8])) {
+	if (row === column && allEqual(board[0], board[4], board[8])) {
 		return currentPlayer;
 	}
 
-	if (row + column === 2 && areEqual(board[2], board[4], board[6])) {
+	if (row + column === 2 && allEqual(board[2], board[4], board[6])) {
 		return currentPlayer;
 	}
 
@@ -52,6 +51,6 @@ export function determineWinner({ board, currentPlayer, lastPlayedAt }) {
  * @param {unknown} b
  * @param {unknown} c
  */
-function areEqual(a, b, c) {
+function allEqual(a, b, c) {
 	return a === b && b === c;
 }
